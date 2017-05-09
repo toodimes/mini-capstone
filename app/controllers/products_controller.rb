8d53919cf1058cc05101b441a3ebb100607401c6
David Astor <toodimes@gmail.com>
2017-05-09 18:45:40 -0400

    add page to view current products, db:seed has code to add products using faker

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,11 @@
 class ProductsController < ApplicationController
+
+  def product_method
+
+    @products = Product.all
+
+    render "product_view.html.erb"
+  end
+
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do 
+  Product.create(name: Faker::Commerce.product_name, price: Faker::Commerce.price.to_s, description: Faker::StarWars.quote, quantity: Faker::Number.between(1, 9999))
+end
+puts "Done!"
