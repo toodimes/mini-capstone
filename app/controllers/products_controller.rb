54dbb4bf3ed27d9f96fd72e1ddcede4bbb2cc673
David Astor <toodimes@gmail.com>
2017-05-10 19:40:10 -0400

    Add form to create a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,5 +7,16 @@ class ProductsController < ApplicationController
     render "product_view.html.erb"
   end
 
+  def new_product_method
+    render "create_new_product.html.erb"
+  end
+
+  def new_product_result_method
+    Product.create(name: params[:name], price: params[:price], image: params[:image], description: params[:description], quantity: params[:quantity])
+
+    @products = Product.all
+
+    render "product_view.html.erb"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get "/products", to: 'products#product_method'
+
+  get "/new_product", to: 'products#new_product_method'
+  post "/new_product_result", to: 'products#new_product_result_method'
 end
